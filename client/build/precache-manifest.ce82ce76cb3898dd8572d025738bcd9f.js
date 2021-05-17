a95389483373c4a8283cc0a70c4ba9a8ac8961be
Esther Cho <eunsem.64642@gmail.com>
2021-05-17 14:32:11 -0600

    let's try again

---
+++ client/build/precache-manifest.ce82ce76cb3898dd8572d025738bcd9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ee592b69498ab4e0247c0b22748798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e081af1bfbee1a575fb1",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "563812fdcfa95d36e049",
+    "url": "/static/js/2.dbbf1802.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.dbbf1802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e081af1bfbee1a575fb1",
+    "url": "/static/js/main.bad650fe.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
